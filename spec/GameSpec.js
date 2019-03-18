7995e8c60b8716366ee84be551511cba095aa83c
Oliver <ofcharrison@gmail.com>
2019-03-18 10:57:37 +0000

    can bowl

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,5 +11,15 @@ beforeEach(function() {
     expect(game._totalScore).toEqual(0)
   });
 
+  it('expect score to be 0', function () {
+    game.roll(0)
+    expect(game._totalScore).toEqual(0)
+  });
+
+  it('expect turns to be 2', function () {
+    game.roll(2);
+    game.roll(3);
+    expect(game.turn).toEqual(2);
+  });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,11 @@
 function Game() {
 this._totalScore = 0;
+this.turn = 0
 };
 
 Game.prototype.roll = function (pins) {
- 
+  this._totalScore += pins
+  this.turn ++
 };
 
 Game.prototype.score = function () {
